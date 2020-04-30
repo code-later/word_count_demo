91782eef2f861f03d1022468822c5105e86c4832
Dirk Breuer <1948+code-later@users.noreply.github.com>
2020-04-30 11:55:46 +0200

    Add test skeleton and proper implementation (#2)

---
+++ __tests__/word_counter.test.js
@@ -0,0 +1,34 @@
+const { WordCounter } = require('../lib/word_counter');
+
+let subject;
+
+beforeEach(() => {
+  subject = new WordCounter()
+});
+
+test('count words in text', () => {
+  const input = "No, no, no, I kill the bus driver.";
+
+  expect(subject.countWords(input)).toEqual([
+    ['no', 3],
+    ['i', 1],
+    ['kill', 1],
+    ['the', 1],
+    ['bus', 1],
+    ['driver', 1]
+  ]);
+});
+
+test('ignore case during count', () => {
+  const input = 'No, no'
+
+  expect(subject.countWords(input)).toEqual([
+    ['no', 2]
+  ])
+})
+
+test('handle empty input', () => {
+  const input = ''
+
+  expect(subject.countWords(input)).toEqual([])
+})
